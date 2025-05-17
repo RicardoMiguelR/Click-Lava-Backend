09a76977f94940bb36c8a434531cf9e257d0ee31
jesusgr <jesusgr9511@gmail.com>
2025-05-17 00:45:24 -0600

    Se crean scripts en tabla tarjeta

--- scriptInserts.sql
+++ scriptInserts.sql
@@ -5,8 +5,16 @@ INSERT INTO Rol (`nombreRol`) VALUES ('cliente');
 
 -- INSERT Usuario
 SELECT * FROM usuario;
-INSERT INTO usuario VALUES(null, "Peter", "Parker", "pparker@email.com", "RGUpp/kYdAPZ8U/J/snzvrtpQuHHijz4oDxwHf3SaJuDzxkGKEm0aOyFqdDRqRRmEZLvMPBddBzCVnVLh9v0AA==", "5512345678", "2025-05-16 00:00:00", 1);
-INSERT INTO usuario VALUES(null, "Ruby", "Sunday", "rsunday@email.com", "FX1uK8PqQGDCfqKaNozGgLAtlrfmlQReR+gtAmwpp7GLwn2o3HuJXG6j2Nzujt6dg4BlKDZijf70zIn/+RRbDw==", "5509876543", "2025-05-16 00:00:00", 2);
-INSERT INTO usuario VALUES(null, "Jack", "Harkness", "jharkness@email.com", "6/Pt5gtPUoZ4pJlsSg+5wppgJQUyhu6sSUBcF39fRRegbV1BUxTImJslEnaf2PNiz9L/vphXtwBpXCgg09Xj/g==", "5512345678", "2025-05-16 00:00:00", 2);
-INSERT INTO usuario VALUES(null, "Rose", "Taylor", "rtaylor@email.com", "kje+jCXn0FLrB9hzKzLc+sbMTsVInYPrrz5iyqkaKZaBbbCGkrknwPtlTK7d09sg54sJBURUDih3SXLQMXqcLw==", "5524681357", "2025-05-16 00:00:00", 2);
-INSERT INTO usuario VALUES(null, "Clara", "Oswald", "coswald@email.com", "2NnAUfxdAv/9Qu30I64smIVNlYbPGVUui2f0I+oRco1/Nu4vEgnjLriWN8kXHGnN7P0HiEj9yZQkvl7j2aLsIA==", "5597532468", "2025-05-16 00:00:00", 1);
+INSERT INTO usuario VALUES(1, "Peter", "Parker", "pparker@email.com", "RGUpp/kYdAPZ8U/J/snzvrtpQuHHijz4oDxwHf3SaJuDzxkGKEm0aOyFqdDRqRRmEZLvMPBddBzCVnVLh9v0AA==", "5512345678", "2025-05-16 00:00:00", 1);
+INSERT INTO usuario VALUES(2, "Ruby", "Sunday", "rsunday@email.com", "FX1uK8PqQGDCfqKaNozGgLAtlrfmlQReR+gtAmwpp7GLwn2o3HuJXG6j2Nzujt6dg4BlKDZijf70zIn/+RRbDw==", "5509876543", "2025-05-16 00:00:00", 2);
+INSERT INTO usuario VALUES(3, "Jack", "Harkness", "jharkness@email.com", "6/Pt5gtPUoZ4pJlsSg+5wppgJQUyhu6sSUBcF39fRRegbV1BUxTImJslEnaf2PNiz9L/vphXtwBpXCgg09Xj/g==", "5512345678", "2025-05-16 00:00:00", 2);
+INSERT INTO usuario VALUES(4, "Rose", "Taylor", "rtaylor@email.com", "kje+jCXn0FLrB9hzKzLc+sbMTsVInYPrrz5iyqkaKZaBbbCGkrknwPtlTK7d09sg54sJBURUDih3SXLQMXqcLw==", "5524681357", "2025-05-16 00:00:00", 2);
+INSERT INTO usuario VALUES(5, "Clara", "Oswald", "coswald@email.com", "2NnAUfxdAv/9Qu30I64smIVNlYbPGVUui2f0I+oRco1/Nu4vEgnjLriWN8kXHGnN7P0HiEj9yZQkvl7j2aLsIA==", "5597532468", "2025-05-16 00:00:00", 1);
+
+-- INSERT Tarjeta
+SELECT * FROM tarjeta;
+INSERT INTO tarjeta VALUES(01, "Peter Parker", "5656343456789098", "12/25", "245", 1); 
+INSERT INTO tarjeta VALUES(02, "Ruby Sunday", "9384756823405698", "09/26", "643", 2);
+INSERT INTO tarjeta VALUES(03, "Jack Harkness", "9384567150987563", "05/27", "459", 3);
+INSERT INTO tarjeta VALUES(04, "Rose Taylor", "5039494555959567", "09/26", "345", 4);
+INSERT INTO tarjeta VALUES(05, "Clara Oswald", "5465360929323335", "04/28", "184", 5);
