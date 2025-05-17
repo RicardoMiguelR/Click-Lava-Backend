ecf0af771d34284975b36da04edd0a8beb14276e
emj <eduard.maju@gmail.com>
2025-05-16 19:05:30 -0600

    Se insertan usuarios

---
+++ scriptInserts.sql
@@ -0,0 +1,12 @@
+-- INSERT Roles
+SELECT * FROM rol;
+INSERT INTO `clic_lava`.`Rol` (`nombreRol`) VALUES ('administrador');
+INSERT INTO `clic_lava`.`Rol` (`nombreRol`) VALUES ('cliente');
+
+-- INSERT Usuario
+SELECT * FROM usuario;
+INSERT INTO usuario VALUES(null, "Peter", "Parker", "pparker@email.com", "RGUpp/kYdAPZ8U/J/snzvrtpQuHHijz4oDxwHf3SaJuDzxkGKEm0aOyFqdDRqRRmEZLvMPBddBzCVnVLh9v0AA==", "5512345678", "2025-05-16 00:00:00", 3);
+INSERT INTO usuario VALUES(null, "Ruby", "Sunday", "rsunday@email.com", "FX1uK8PqQGDCfqKaNozGgLAtlrfmlQReR+gtAmwpp7GLwn2o3HuJXG6j2Nzujt6dg4BlKDZijf70zIn/+RRbDw==", "5509876543", "2025-05-16 00:00:00", 4);
+INSERT INTO usuario VALUES(null, "Jack", "Harkness", "jharkness@email.com", "6/Pt5gtPUoZ4pJlsSg+5wppgJQUyhu6sSUBcF39fRRegbV1BUxTImJslEnaf2PNiz9L/vphXtwBpXCgg09Xj/g==", "5512345678", "2025-05-16 00:00:00", 4);
+INSERT INTO usuario VALUES(null, "Rose", "Taylor", "rtaylor@email.com", "kje+jCXn0FLrB9hzKzLc+sbMTsVInYPrrz5iyqkaKZaBbbCGkrknwPtlTK7d09sg54sJBURUDih3SXLQMXqcLw==", "5524681357", "2025-05-16 00:00:00", 4);
+INSERT INTO usuario VALUES(null, "Clara", "Oswald", "coswald@email.com", "2NnAUfxdAv/9Qu30I64smIVNlYbPGVUui2f0I+oRco1/Nu4vEgnjLriWN8kXHGnN7P0HiEj9yZQkvl7j2aLsIA==", "5597532468", "2025-05-16 00:00:00", 3);
