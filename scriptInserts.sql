4b1e6cbad2465f388812ace20790219f9a0b1d40
anagabriela <anagarcia.comunicacion@gmail.com>
2025-05-17 17:07:25 -0600

    Se sube pedido

--- scriptInserts.sql
+++ scriptInserts.sql
@@ -18,3 +18,11 @@ INSERT INTO tarjeta VALUES(02, "Ruby Sunday", "9384756823405698", "09/26", "643"
 INSERT INTO tarjeta VALUES(03, "Jack Harkness", "9384567150987563", "05/27", "459", 3);
 INSERT INTO tarjeta VALUES(04, "Rose Taylor", "5039494555959567", "09/26", "345", 4);
 INSERT INTO tarjeta VALUES(05, "Clara Oswald", "5465360929323335", "04/28", "184", 5);
+
+-- INSERT Pedido
+SELECT * FROM pedido;
+INSERT INTO pedido VALUES (1,"Av. Chimalhuacán 12","Benito Juárez" ,"Nezahualcoyotl", "5700", "2025-05-19 06:00:00", 1,"Un día", 1);
+INSERT INTO pedido VALUES (2,"Calle 20 de noviembre","Las Águilas" ,"Nezahualcoyotl", "5700", "2025-05-20 08:00:00", 1, "Un día", 2);
+INSERT INTO pedido VALUES (3,"Av. Pantitlán 300","Benito Juárez" ,"Nezahualcoyotl", "5700", "2025-05-22 09:00:00", 1, "Un día", 3);
+INSERT INTO pedido VALUES (4,"Calle Vicente Gerrero 89","Benito Juárez" ,"Nezahualcoyotl", "5700", "2025-05-23 12:00:00", 1, "Un día", 4);
+INSERT INTO pedido VALUES (5,"Av. Riva Palacio 150","Benito Juárez" ,"Nezahualcoyotl", "5700", "2025-05-23 7:00:00", 1, "Un día", 5);
