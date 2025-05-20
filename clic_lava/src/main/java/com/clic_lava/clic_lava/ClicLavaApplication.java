d54169b3633d4ed2130b91b7323be1ccd7c2b676
Raymundo Mendoza Ramirez <raymundomendozaramirez@gmail.com>
2025-05-20 12:37:05 -0600

    Se agrega proyecto clic_lava

---
+++ clic_lava/src/main/java/com/clic_lava/clic_lava/ClicLavaApplication.java
@@ -0,0 +1,13 @@
+package com.clic_lava.clic_lava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClicLavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClicLavaApplication.class, args);
+	}
+
+}
---
+++ clic_lava/src/test/java/com/clic_lava/clic_lava/ClicLavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clic_lava.clic_lava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClicLavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
