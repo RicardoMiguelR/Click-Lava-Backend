573f12ee76a98bd6f90695da7a5be3696a3dd248
Raymundo Mendoza Ramirez <71798777+R1ymundo@users.noreply.github.com>
2025-05-20 18:08:38 -0600

    Se actualiza RolService.java

--- clicLava/src/main/java/org/clicLava/service/RolService.java
+++ clicLava/src/main/java/org/clicLava/service/RolService.java
@@ -1,5 +1,64 @@
 package org.clicLava.service;
 
-public class RolService {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.clicLava.model.Rol;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RolService {
+    private final List<Rol> listaRoles = new ArrayList<Rol>();
+    
+    @Autowired
+    public RolService() {
+        listaRoles.add(new Rol("administrador"));
+        listaRoles.add(new Rol("cliente"));
+
+    }
+    
+    public List<Rol> getRoles() {
+        return listaRoles;
+    }
+    
+    public Rol getRol(Long id) {
+        Rol tmp = null;
+        for (Rol rol : listaRoles) {
+            if (rol.getId() == id) {
+                tmp = rol;
+                break;
+            }
+        }
+        return tmp;
+    }
+    
+    public Rol deleteRol(Long id) {
+        Rol tmp = null;
+        for (Rol rol : listaRoles) {
+            if (rol.getId() == id) {
+                tmp = rol;
+                listaRoles.remove(rol);
+                break;
+            }
+        }
+        return tmp;
+    }
+    
+    public Rol addRol(Rol rol) {
+        listaRoles.add(rol);
+        return rol;
+    }
+    
+    public Rol updateRol(Long id, String nombreRol) {
+        Rol tmp = null;
+        for (Rol rol : listaRoles) {
+            if (rol.getId() == id) {
+                if(nombreRol != null) rol.setRol(nombreRol);
+                tmp = rol;
+                break;
+            }
+        }
+        return tmp;
+    }
 }
